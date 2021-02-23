245b71dfcd03cf8089c7eca80f2d8cfcfdfe9fb6
yeji111 <59949675+yeji111@users.noreply.github.com>
2021-02-23 12:41:26 +0900

    Add files via upload

--- Tmass.C
+++ Tmass.C
@@ -9,6 +9,9 @@
 #include "TStyle.h"
 #include <vector>
 
+using namespace ROOT::VecOps;
+using rvec_i = RVec<int>;
+
 template <typename T>
 void plot(T hist, TString name){
     TCanvas *c = new TCanvas("c",Form("c_%s", name.Data()));
@@ -17,83 +20,152 @@ void plot(T hist, TString name){
     c->Print(Form("%s.pdf",name.Data()));
 }
 
+int goodleading_idx(rvec_i good){
+    int idx = -1;
+    for( int i = 0; i < good.size(); i++){
+        if(good[i]) idx = i;
+        break;
+    }
+    return idx;
+}
+
 
 void Tmass(){
+	ROOT::RDataFrame df_166("Events", {"/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop166p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/240000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop166p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/*.root"});
+	ROOT::RDataFrame df_169("Events", {"/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop169p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/280000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop169p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/10000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop169p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop169p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/240000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop169p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/250000/*.root"});
+	ROOT::RDataFrame df_171("Events", {"/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop171p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/60000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop171p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/240000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop171p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/*.root"});
+	ROOT::RDataFrame df_173("Events", {"/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop173p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/60000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop173p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/260000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop173p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/250000/*.root"});
+	ROOT::RDataFrame df_175("Events", {"/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop175p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/60000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop175p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/130000/*.root"});
+	ROOT::RDataFrame df_178("Events", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop178p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/60000/*.root");
+  
+    auto df_s0_166 = df_166.Filter("HLT_IsoMu20","SingleMuon Trigger");
+    auto df_s0_169 = df_169.Filter("HLT_IsoMu20","SingleMuon Trigger");
+    auto df_s0_171 = df_171.Filter("HLT_IsoMu20","SingleMuon Trigger");
+    auto df_s0_173 = df_173.Filter("HLT_IsoMu20","SingleMuon Trigger");
+    auto df_s0_175 = df_175.Filter("HLT_IsoMu20","SingleMuon Trigger");
+    auto df_s0_178 = df_178.Filter("HLT_IsoMu20","SingleMuon Trigger");
 
-        std::vector<string> obj = {"Muon1_pt"};
-
-
-	ROOT::RDataFrame df_signal("Events", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop175p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/130000/*.root");
-	ROOT::RDataFrame df_bg("Events", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/250000/*.root");
-//	ROOT::RDataFrame df_fake("Events", {"/xrootd/store/mc/RunIIAutumn18NanoAODv6/W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/250000/*.root", "/xrootd/store/mc/RunIIAutumn18NanoAODv6/TTToSemiLeptonic_mtop175p5_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/130000/*.root"});
-    
-//   double L = 59.7;
-//   double cs_sig = 831.;
-//   double cs_bg = 544.3;
-//   L*cs_sig = 49610700;
-
-//    auto df_s0_fake = df_fake.Filter("HLT_IsoMu20","SingleMuon Trigger");
-    auto df_s0_signal = df_signal.Range(49610700).Filter("HLT_IsoMu20","SingleMuon Trigger");
-    auto df_s0_bg = df_bg.Filter("HLT_IsoMu20","SingleMuon Trigger");
-
-    auto n_signal = df_signal.Count();
-    auto n_bg = df_bg.Count();
-    cout << " n. signal = " << *n_signal << " n. bg = " << *n_bg << endl;
- 
     //**** Muon Selection ( tight ) ****//
-    auto df_s1_signal = df_s0_signal.Define("goodMuon","Muon_pt >= 20 && abs(Muon_eta) < 2.0 && Muon_tightId && Muon_pfRelIso04_all < 0.15")
-                                    .Define("n_goodMuon","Sum(goodMuon)")
-                                    .Filter("n_goodMuon == 1","Muon Selection");
-    auto df_s1_bg = df_s0_bg.Define("goodMuon","Muon_pt >= 20 && abs(Muon_eta) < 2.0 && Muon_tightId && Muon_pfRelIso04_all < 0.15")
-                            .Define("n_goodMuon","Sum(goodMuon)")
-                            .Filter("n_goodMuon == 1","Muon Selection");
+    auto df_s1_166 = df_166.Define("goodMuon","Muon_pt >= 30 && abs(Muon_eta) < 2.5 && Muon_tightId && Muon_pfRelIso04_all < 0.15")
+                              .Define("n_goodMuon","Sum(goodMuon)")
+                              .Filter("n_goodMuon == 1","Muon Selection")
+			      .Define("goodMuon_leading_idx",goodleading_idx,{"goodMuon"});
+    auto df_s1_169 = df_169.Define("goodMuon","Muon_pt >= 30 && abs(Muon_eta) < 2.5 && Muon_tightId && Muon_pfRelIso04_all < 0.15")
+                              .Define("n_goodMuon","Sum(goodMuon)")
+                              .Filter("n_goodMuon == 1","Muon Selection")
+		              .Define("goodMuon_leading_idx",goodleading_idx,{"goodMuon"});
+    auto df_s1_171 = df_171.Define("goodMuon","Muon_pt >= 30 && abs(Muon_eta) < 2.5 && Muon_tightId && Muon_pfRelIso04_all < 0.15")
+                              .Define("n_goodMuon","Sum(goodMuon)")
+                              .Filter("n_goodMuon == 1","Muon Selection")
+	                      .Define("goodMuon_leading_idx",goodleading_idx,{"goodMuon"});
+    auto df_s1_173 = df_173.Define("goodMuon","Muon_pt >= 30 && abs(Muon_eta) < 2.5 && Muon_tightId && Muon_pfRelIso04_all < 0.15")
+                              .Define("n_goodMuon","Sum(goodMuon)")
+                              .Filter("n_goodMuon == 1","Muon Selection")
+			      .Define("goodMuon_leading_idx",goodleading_idx,{"goodMuon"});
+    auto df_s1_175 = df_175.Define("goodMuon","Muon_pt >= 30 && abs(Muon_eta) < 2.5 && Muon_tightId && Muon_pfRelIso04_all < 0.15")
+                              .Define("n_goodMuon","Sum(goodMuon)")
+                              .Filter("n_goodMuon == 1","Muon Selection")
+                              .Define("goodMuon_leading_idx",goodleading_idx,{"goodMuon"});
+    auto df_s1_178 = df_178.Define("goodMuon","Muon_pt >= 30 && abs(Muon_eta) < 2.5 && Muon_tightId && Muon_pfRelIso04_all < 0.15")
+                              .Define("n_goodMuon","Sum(goodMuon)")
+                              .Filter("n_goodMuon == 1","Muon Selection")
+                              .Define("goodMuon_leading_idx",goodleading_idx,{"goodMuon"});
     
     //**** Jet Selection ( tight ) ****//
-    auto df_s2_signal = df_s1_signal.Define("goodJet","Jet_pt >= 20 && abs(Jet_eta) < 2.0 && Jet_jetId == 6")
-                                    .Define("n_goodJet","Sum(goodJet)")
-                                    .Filter("n_goodJet >= 4","Jet Selection");
-    auto df_s2_bg = df_s1_bg.Define("goodJet","Jet_pt >= 20 && abs(Jet_eta) < 2.0 && Jet_jetId == 6")
-                            .Define("n_goodJet","Sum(goodJet)")
-                            .Filter("n_goodJet >= 4","Jet Selection");
+    auto df_s2_166 = df_s1_166.Define("goodJet","Jet_pt >= 30 && abs(Jet_eta) < 2.5 && Jet_jetId == 6")
+                              .Define("n_goodJet","Sum(goodJet)")
+                              .Filter("n_goodJet >= 4","Jet Selection");
+    auto df_s2_169 = df_s1_169.Define("goodJet","Jet_pt >= 30 && abs(Jet_eta) < 2.5 && Jet_jetId == 6")
+                              .Define("n_goodJet","Sum(goodJet)")
+                              .Filter("n_goodJet >= 4","Jet Selection");
+    auto df_s2_171 = df_s1_171.Define("goodJet","Jet_pt >= 30 && abs(Jet_eta) < 2.5 && Jet_jetId == 6")
+                              .Define("n_goodJet","Sum(goodJet)")
+                              .Filter("n_goodJet >= 4","Jet Selection");
+    auto df_s2_173 = df_s1_173.Define("goodJet","Jet_pt >= 30 && abs(Jet_eta) < 2.5 && Jet_jetId == 6")
+                              .Define("n_goodJet","Sum(goodJet)")
+                              .Filter("n_goodJet >= 4","Jet Selection");
+    auto df_s2_175 = df_s1_175.Define("goodJet","Jet_pt >= 30 && abs(Jet_eta) < 2.5 && Jet_jetId == 6")
+                              .Define("n_goodJet","Sum(goodJet)")
+                              .Filter("n_goodJet >= 4","Jet Selection");
+    auto df_s2_178 = df_s1_178.Define("goodJet","Jet_pt >= 30 && abs(Jet_eta) < 2.5 && Jet_jetId == 6")
+                              .Define("n_goodJet","Sum(goodJet)")
+                              .Filter("n_goodJet >= 4","Jet Selection");
 
     
     //**** b Tagged Jet selection ( medium )****//
-    auto df_s3_signal = df_s2_signal.Define("goodbJet","goodJet && Jet_btagDeepFlavB > 0.277")
-                                    .Define("n_goodbJet","Sum(goodbJet)")
-                                    .Filter("n_goodbJet >= 2","bJet Selection");
-    auto df_s3_bg = df_s2_bg.Define("goodbJet","goodJet && Jet_btagDeepFlavB > 0.277")
-                            .Define("n_goodbJet","Sum(goodbJet)")
-                            .Filter("n_goodbJet >= 2","bJet Selection");
+    auto df_s3_166 = df_s2_166.Define("goodbJet","goodJet && Jet_btagDeepFlavB > 0.277")
+                              .Define("n_goodbJet","Sum(goodbJet)")
+                              .Filter("n_goodbJet >= 2","bJet Selection");
+    auto df_s3_169 = df_s2_169.Define("goodbJet","goodJet && Jet_btagDeepFlavB > 0.277")
+                              .Define("n_goodbJet","Sum(goodbJet)")
+                              .Filter("n_goodbJet >= 2","bJet Selection");
+    auto df_s3_171 = df_s2_171.Define("goodbJet","goodJet && Jet_btagDeepFlavB > 0.277")
+                              .Define("n_goodbJet","Sum(goodbJet)")
+                              .Filter("n_goodbJet >= 2","bJet Selection");
+    auto df_s3_173 = df_s2_173.Define("goodbJet","goodJet && Jet_btagDeepFlavB > 0.277")
+                              .Define("n_goodbJet","Sum(goodbJet)")
+                              .Filter("n_goodbJet >= 2","bJet Selection");
+    auto df_s3_175 = df_s2_175.Define("goodbJet","goodJet && Jet_btagDeepFlavB > 0.277")
+                              .Define("n_goodbJet","Sum(goodbJet)")
+                              .Filter("n_goodbJet >= 2","bJet Selection");
+    auto df_s3_178 = df_s2_178.Define("goodbJet","goodJet && Jet_btagDeepFlavB > 0.277")
+                              .Define("n_goodbJet","Sum(goodbJet)")
+                              .Filter("n_goodbJet >= 2","bJet Selection");
+
 
    //***** MET selection *****//
-   auto df_s4_signal = df_s3_signal.Filter("MET_pt >= 20", "MET Selection");
-   auto df_s4_bg = df_s3_bg.Filter("MET_pt >= 20", "MET Selection");
+   auto df_s4_166 = df_s3_166.Filter("MET_pt >= 20", "MET Selection");
+   auto df_s4_169 = df_s3_169.Filter("MET_pt >= 20", "MET Selection");
+   auto df_s4_171 = df_s3_171.Filter("MET_pt >= 20", "MET Selection");
+   auto df_s4_173 = df_s3_173.Filter("MET_pt >= 20", "MET Selection");
+   auto df_s4_175 = df_s3_175.Filter("MET_pt >= 20", "MET Selection");
+   auto df_s4_178 = df_s3_178.Filter("MET_pt >= 20", "MET Selection");
+    
+   //***** W boson transverse mass selection *****//
+   auto df_s5_166 = df_s4_166.Define("WTmass","sqrt(Muon_pt[goodMuon_leading_idx]*MET_pt*(1-cos(Muon_phi[goodMuon_leading_idx]-MET_phi)))")
+                             .Filter("(WTmass+MET_pt)>=60", "WTmass Selection");
+   auto df_s5_169 = df_s4_169.Define("WTmass","sqrt(Muon_pt[goodMuon_leading_idx]*MET_pt*(1-cos(Muon_phi[goodMuon_leading_idx]-MET_phi)))")
+                             .Filter("(WTmass+MET_pt)>=60", "WTmass Selection");
+   auto df_s5_171 = df_s4_171.Define("WTmass","sqrt(Muon_pt[goodMuon_leading_idx]*MET_pt*(1-cos(Muon_phi[goodMuon_leading_idx]-MET_phi)))")
+                             .Filter("(WTmass+MET_pt)>=60", "WTmass Selection");
+   auto df_s5_173 = df_s4_173.Define("WTmass","sqrt(Muon_pt[goodMuon_leading_idx]*MET_pt*(1-cos(Muon_phi[goodMuon_leading_idx]-MET_phi)))")
+                             .Filter("(WTmass+MET_pt)>=60", "WTmass Selection");
+   auto df_s5_175 = df_s4_175.Define("WTmass","sqrt(Muon_pt[goodMuon_leading_idx]*MET_pt*(1-cos(Muon_phi[goodMuon_leading_idx]-MET_phi)))")
+                             .Filter("(WTmass+MET_pt)>=60", "WTmass Selection");
+   auto df_s5_178 = df_s4_178.Define("WTmass","sqrt(Muon_pt[goodMuon_leading_idx]*MET_pt*(1-cos(Muon_phi[goodMuon_leading_idx]-MET_phi)))")
+                             .Filter("(WTmass+MET_pt)>=60", "WTmass Selection");
 
-
-   //***** Muon_pt Definition *****//    
-   auto df_s5_signal = df_s4_signal.Define("Muon1_pt","Muon_pt[goodMuon][0]");
-   auto df_s5_bg = df_s4_bg.Define("Muon1_pt","Muon_pt[goodMuon][0]");
-   
     //histograms 
-    auto h_muon_pt_signal = df_s5_signal.Histo1D({"h_muon_pt_signal", "h_muon_pt_signal", 50, 0, 250}, "Muon1_pt");
-    auto h_muon_pt_bg = df_s5_bg.Histo1D({"h_muon_pt_bg", "h_muon_pt_bg", 50, 0, 250}, "Muon1_pt");
-
-    auto h_event_signal = df_signal.Histo1D({"h_event_signal","h_event_signal",10,0,10},"nMuon");
-    auto h_event_bg = df_bg.Histo1D({"h_event_bg","h_event_bg",10,0,10},"nMuon");
-
-    //ntuple
-    df_s5_signal.Snapshot("tree","ntuple_s.root",obj);
-    df_s5_bg.Snapshot("tree","ntuple_b.root",obj);
-          
-
+    auto h_muon_pt_166 = df_s5_166.Define("Muon1_pt","Muon_pt[goodMuon]").Histo1D({"h_muon_pt_166", "h_muon_pt_166", 24, 30, 150}, "Muon1_pt");
+    auto h_muon_pt_169 = df_s5_169.Define("Muon1_pt","Muon_pt[goodMuon]").Histo1D({"h_muon_pt_169", "h_muon_pt_169", 24, 30, 150}, "Muon1_pt");
+    auto h_muon_pt_171 = df_s5_171.Define("Muon1_pt","Muon_pt[goodMuon]").Histo1D({"h_muon_pt_171", "h_muon_pt_171", 24, 30, 150}, "Muon1_pt");
+    auto h_muon_pt_173 = df_s5_173.Define("Muon1_pt","Muon_pt[goodMuon]").Histo1D({"h_muon_pt_173", "h_muon_pt_173", 24, 30, 150}, "Muon1_pt");
+    auto h_muon_pt_175 = df_s5_175.Define("Muon1_pt","Muon_pt[goodMuon]").Histo1D({"h_muon_pt_175", "h_muon_pt_175", 24, 30, 150}, "Muon1_pt");
+    auto h_muon_pt_178 = df_s5_178.Define("Muon1_pt","Muon_pt[goodMuon]").Histo1D({"h_muon_pt_178", "h_muon_pt_178", 24, 30, 150}, "Muon1_pt");
+/*
+    auto h_event_166 = df_166.Histo1D({"h_event_166","h_event_166",10,0,10},"nMuon");
+    auto h_event_169 = df_169.Histo1D({"h_event_169","h_event_169",10,0,10},"nMuon");
+    auto h_event_171 = df_171.Histo1D({"h_event_171","h_event_171",10,0,10},"nMuon");
+    auto h_event_173 = df_173.Histo1D({"h_event_173","h_event_173",10,0,10},"nMuon");
+    auto h_event_175 = df_175.Histo1D({"h_event_175","h_event_175",10,0,10},"nMuon");
+    auto h_event_178 = df_178.Histo1D({"h_event_178","h_event_178",10,0,10},"nMuon");
+*/ 
     TFile f("out.root", "recreate");
- 
-    plot(h_muon_pt_signal, "h_muon_pt_signal");
-    plot(h_muon_pt_bg, "h_muon_pt_bg");
 
-    plot(h_event_signal, "h_event_signal");
-    plot(h_event_bg, "h_event_bg");
-
+    plot(h_muon_pt_166, "h_muon_pt_166");
+    plot(h_muon_pt_169, "h_muon_pt_169");
+    plot(h_muon_pt_171, "h_muon_pt_171");
+    plot(h_muon_pt_173, "h_muon_pt_173");
+    plot(h_muon_pt_175, "h_muon_pt_175");
+    plot(h_muon_pt_178, "h_muon_pt_178");
+/*
+    plot(h_event_166, "h_event_166");
+    plot(h_event_169, "h_event_169");
+    plot(h_event_171, "h_event_171");
+    plot(h_event_173, "h_event_173");
+    plot(h_event_175, "h_event_175");
+    plot(h_event_178, "h_event_178");
+*/
     f.Close();
 
 
--- fit.C
+++ fit.C
@@ -5,26 +5,24 @@ void makeFit(TFile *f, TString name){
 
    TH1F *h1 = (TH1F*) f->Get(Form("h_muon_pt_%s",name.Data()));
    gStyle->SetOptFit(1);
-   double ne = h1-> Integral();
-   
+
    TCanvas * c1 = new TCanvas(Form("c1_%s",name.Data()),"c",1);   
-   TF1 *f1 = new TF1("f1","[2]*5*TMath::GammaDist(x,1.+[0],0,[1])/(1+exp((20-x)/0.1))",0,250);
-   f1->SetParNames("p","q","number of events");
-   f1->SetParameter(0,1.5); 
-   f1->SetParameter(1,20.); 
-   f1->SetParameter(2,ne); 
-   h1->Fit("f1","0","",20, 250);
-   h1->Draw("E, HIST");
-   h1->GetXaxis()->SetTitle("P_{T} [GeV]");
-   h1->GetYaxis()->SetTitle("Entries");
-   f1->Draw("same");
-   c1->Print(Form("fit_h_muon_pt_%s.pdf",name.Data()));
+   TF1 *f1 = new TF1("f1","[0]*TMath::Exp(-0.5*TMath::Power((TMath::Log(1+(x-[1])*TMath::SinH([2]*TMath::Sqrt(TMath::Log(4)))/([3]*TMath::Sqrt(TMath::Log(4))))/[2]),2)+TMath::Power([2],2))",30,150);
+   f1->SetParNames("normalisation","mu","tau","sigma");
+   f1->FixParameter(0,0.12); 
+   f1->FixParameter(1,26); 
+   f1->FixParameter(2,22);
+   f1->FixParameter(2,0.44);
+  // h1->Fit("f1","0","",20, 250);
+  // h1->Draw("HIST");
+  // h1->GetXaxis()->SetTitle("P_{T} [GeV]");
+  // h1->GetYaxis()->SetTitle("Entries");
+   f1->Draw();//"same");
+  // c1->Print(Form("fit_h_muon_pt_%s.pdf",name.Data()));
 
-   cout<< "Integral = "<< ne <<endl;
-
-   TFile fit("fit.root","update");
-   f1->Write(Form("fit_h_muon_pt_%s",name.Data()));
-   fit.Close();
+   //TFile fit("fit.root","update");
+   //f1->Write(Form("fit_h_muon_pt_%s",name.Data()));
+   //fit.Close();
    
 }
 
@@ -32,10 +30,5 @@ void fit(){
 
    TFile *f_muon = new TFile("out.root");
 
-   makeFit(f_muon, "166");
-   makeFit(f_muon, "169");
    makeFit(f_muon, "171");
-   makeFit(f_muon, "173");
-   makeFit(f_muon, "175");
-   makeFit(f_muon, "178");
 }
