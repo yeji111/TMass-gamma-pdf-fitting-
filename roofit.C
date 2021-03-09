3ad0c38d181a00583469f0cae82aba86a14146ed
yeji111 <59949675+yeji111@users.noreply.github.com>
2021-03-09 16:58:43 +0900

    Add files via upload

--- roofit.C
+++ roofit.C
@@ -16,54 +16,52 @@ void makeFit(TFile *f){
    RooRealVar Muon1_pt("Muon1_pt", "P_{T} (GeV)", 30, 150);
    RooDataHist data("data","dataset with muon_pt",Muon1_pt,h); 
 
-   cout << "n. fake = " << h->GetEntries() << endl;
-
-   //// Set variables of Constraints ////
-   RooRealVar num("num","number of fake data",h->GetEntries());
+   cout << "n. fake = " << h->Integral() << endl;
 
    //// Set Variables of model function ////
-   RooRealVar mass("mass", "mass", 30, 150);
-   RooRealVar ni("ni","ni", 0.212);
-   RooRealVar ns("ns","ns", -0.0006778);
-   RooRealVar mui("mui","mui", 3.88);
-   RooRealVar mus("mus","mus", 0.1112);
-   RooRealVar sigi("sigi","sigi", -0.4083);
-   RooRealVar sigs("sigs","sigs", 0.1378);
-   RooRealVar taui("taui","taui", 0.7515);
-   RooRealVar taus("taus","taus", -0.001007);
+   RooRealVar mass("mass", "mass", 150, 200);
+   RooRealVar normalization("normalization","normalization", 0.1);
+   RooRealVar mui("mui","mui", 50.2);
+   RooRealVar mus("mus","mus", -0.1686);
+   RooRealVar sigi("sigi","sigi", -19.91);
+   RooRealVar sigs("sigs","sigs", 0.2555);
+   RooRealVar taui("taui","taui", 1.257);
+   RooRealVar taus("taus","taus", -0.004062);
 
-   RooFormulaVar normalization("normalization", "ni+mass*ns", RooArgList(ni,mass,ns)); 
    RooFormulaVar mu("mu","mui+mass*mus", RooArgList(mui,mass,mus)); 
    RooFormulaVar sigma("sigma","sigi+mass*sigs", RooArgList(sigi,mass,sigs)); 
    RooFormulaVar tau("tau","taui+mass*taus", RooArgList(taui,mass,taus)); 
   
-   RooGenericPdf lamda("lamda", "normalization*exp(-0.5*pow(((log(1+(Muon1_pt-mu)*sinh(tau*sqrt(log(4)))/(sigma*sqrt(log(4)))))/tau),2)+pow(tau,2))", RooArgList(normalization, Muon1_pt, mu, tau, sigma));
-
-   //// Make model function ////
-   RooPoisson model("model", "poisson_constraints", num, lamda);
-
+   RooGenericPdf lambda("lambda", "normalization*exp(-0.5*pow(((log(1+(Muon1_pt-mu)*sinh(tau*sqrt(log(4)))/(sigma*sqrt(log(4)))))/tau),2)+pow(tau,2))", RooArgList(normalization, Muon1_pt, mu, tau, sigma));
+   
+   //// create binned dataset & Pdf ////
+   RooDataSet *lam_data = lambda.generate(Muon1_pt, 10000);
+   TH1 *h1_lam = lam_data->createHistogram("Muon1_pt", 24);
+   RooDataHist hist_lam("hist_lam","hist_lam", Muon1_pt, Import(*h1_lam));
+   RooHistPdf lam_pdf("lam_pdf", "lam_pdf", Muon1_pt, hist_lam); 
+   
    //// Fitting data to model ////
    RooPlot *frame = Muon1_pt.frame();
-   RooFitResult* result = model.fitTo(data, Range(30,150), Save());
+   RooFitResult* result = lam_pdf.fitTo(data, Range(30,150), Save());
    data.plotOn(frame);
-   model.plotOn(frame, LineColor(kBlue));
-
+   lam_pdf.plotOn(frame, LineColor(kBlue));
+   
    //// Fitting results ////
    mass.Print();
    result->Print();
 
    //// create NLL ////
-   RooAbsReal *nll = model.createNLL(data);
+   RooAbsReal *nll = lam_pdf.createNLL(data);
    RooPlot *frame_nll = mass.frame(Range(165, 185));
    nll->plotOn(frame_nll, ShiftToZero());
    frame_nll -> SetMinimum(0);
    frame_nll -> SetMaximum(3);
-
-   //// create distribution subtracted ////
+ 
+  //// create distribution subtracted ////
    RooPlot *frame_sub = mass.frame(Range(165, 185));
    RooRealVar real_mass("real_mass", "real_mass", 172.5);
    real_mass.plotOn(frame_sub, ShiftToZero(), LineColor(kGreen));
-   RooFormulaVar sub("sub","mass-real_mass", RooArgList(mass, real_mass)); 
+   RooFormulaVar sub("sub", "mass-real_mass", RooArgList(mass, real_mass));
    sub.plotOn(frame_sub);
    frame_sub -> SetMinimum(-4);
    frame_sub -> SetMaximum(4);
